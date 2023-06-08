155a9eba9f29a954d41a813ea53e41545fef7632
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-06-08 15:10:20 +0200

    nav bar mobile okay

---
+++ templates/navbar.php
@@ -0,0 +1,9 @@
+<header>
+    <nav class="nav">
+        <ul class="ul-nav">
+            <li class="li"><a class="a2" href="index.php">Accueil</a></li>
+            <li class="li"><a class="a2" href="index.php?page=about">Projet</a></li>
+            <li class="li"><a class="a2" href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
