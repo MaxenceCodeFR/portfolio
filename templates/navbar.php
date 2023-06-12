10aecafcb7f46e8372f9fb044ebbecefe408b6a4
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-06-12 17:35:00 +0200

    page contact okey

--- templates/navbar.php
+++ templates/navbar.php
@@ -3,7 +3,7 @@
         <ul class="ul-nav">
             <li class="li"><a class="a2" href="index.php">Accueil</a></li>
             <li class="li"><a class="a2" href="index.php?page=about">Projet</a></li>
-            <li class="li"><a class="a2" href="index.php?page=contact">Contact</a></li>
+            <li class="li"><a class="a2" href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
