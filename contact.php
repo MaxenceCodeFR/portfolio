dd3715932e4141c6a5ddd533765381b3e01fc3d6
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-06-14 10:21:39 +0200

    Calibrage final

--- contact.php
+++ contact.php
@@ -5,45 +5,76 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - Maxence HATTABI</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
    <link rel="stylesheet" href="/style/style.css">
 </head>
 <body>
+    
+    <!-- NAVBAR  -->
     <?php include "navbar.php" ?>
+
+    <!-- TITRE  -->
     <h1 class="h1contact">N'hésitez pas a m'envoyer un message !</h1>
 
     <div class="main">
+    <!-- PAIRE EN HAUT  -->
+        <!-- MAIL  -->
         <div class="up">
-            <a href="mailto:maxence.hattabi@gmail.com" target="_blank">
+        <div data-aos="fade-down-right" data-aos-duration="3000">
+        <a href="mailto:maxence.hattabi@gmail.com" target="_blank">
                 <button class="card1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" class="bi bi-envelope-at-fill mail" viewBox="0 0 30 30">
                     <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
                     <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z"/>
                     </svg>
-                    </button>
+                </button>
             </a>
-            <a href="https://www.linkedin.com/in/maxence-hattabi/" target="_blank">
+
+            <!-- LINKEDIN  -->
+        </div>
+        <div data-aos="fade-down-left" data-aos-duration="3000">
+        <a href="https://www.linkedin.com/in/maxence-hattabi/" target="_blank">
                 <button class="card2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px"  class="bi bi-linkedin linkedin1" viewBox="0 0 16 16">
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                     </svg>
                 </button>
             </a>
+        </div>  
         </div>
+
+        <!-- PAIRE EN BAS  -->
+            <!-- GITHUB  -->
     <div class="down">
-            <a href="https://github.com/MaxenceCodeFR" target="_blank">
-                <button class="card3">
+        <div data-aos="fade-up-right" data-aos-duration="3000">
+        <a href="https://github.com/MaxenceCodeFR" target="_blank">
+                <button class="card3" >
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="40px" height="40px" class="github1">    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg>
                 </button>
             </a>
+
+            <!-- CV -->
+        </div>
+            <div data-aos="fade-up-left" data-aos-duration="3000">
             <a href="/images/cv-hattabi-maxence.pdf" target="_blank">
-                <button class="card4">
+                <button class="card4" >
                     <svg xmlns="http://www.w3.org/2000/svg" width="35px" height="35px" class="bi bi-file-earmark-person-fill cv" viewBox="0 0 16 16">
                     <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755z"/>
                     </svg>
                 </button>
             </a>
+            </div>
+            
   </div>
 </div>
+
+<!-- ADIOS -->
     <h3 class="goodbye">À bientôt ...</h3>
+
+<!-- SCRIPT  -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 </html>
