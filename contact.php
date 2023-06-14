a1e3621ec9b0b0b47cb8dfddb127a7aca950a2f3
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-06-14 10:27:22 +0200

    oubli de favicon

--- contact.php
+++ contact.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/images/nuage.png">
     <title>Accueil - Maxence HATTABI</title>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
    <link rel="stylesheet" href="/style/style.css">
 </head>
 <body>
-    
+
     <!-- NAVBAR  -->
     <?php include "navbar.php" ?>
 
